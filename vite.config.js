543cc8d9e8c44c8cf577af0d2e193c606ca312a5
Shardool Kashyap <shardool@unifynd.com>
2025-06-12 14:43:48 +0530

    Set Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Luxture-product-catalogue-Ant-Design/', 
+  base: '/Luxture-product-catalogue-Ant-Design/',
   plugins: [react()],
 })
